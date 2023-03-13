6ab495522b98df2ca4c9eda16587075a927314aa
v.mikheeva <algaeminor@gmail.com>
2023-03-14 01:25:31 +0400

    2 module 1-task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (let key in salaries) {
+    if (!Number.isNaN(salaries[key]) && Number.isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+  return sum;
 }
