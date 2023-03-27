f9ba52d0ab0aaf430e36b66d8a0f0ef6271116ef
v.mikheeva <algaeminor@gmail.com>
2023-03-27 21:34:15 +0400

    3-module 3-task

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split('-');
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== 0) {
+      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
+    }
+  }
+  return arr.join('');
 }
