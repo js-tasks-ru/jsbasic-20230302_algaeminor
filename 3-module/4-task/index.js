380305fa6915ecd18934a6b964fba37f9a19b757
v.mikheeva <algaeminor@gmail.com>
2023-03-27 23:39:15 +0400

    3-module 4 task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = '';
+  users = users.filter(user => user.age <= age);
+  for (let i = 0; i < users.length; i++) {
+    str += `${users[i].name}, ${users[i].balance}`;
+    if (i + 1 < users.length) {
+      str += '\n';
+    }
+  }
+  return str;
 }
