a69293a91d1354e8539c4e0f67119f68b62ddb3d
v.mikheeva <algaeminor@gmail.com>
2023-03-30 00:32:25 +0400

    map вместо цикла for

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,9 +1,5 @@
 function camelize(str) {
   let arr = str.split('-');
-  for (let i = 0; i < arr.length; i++) {
-    if (i !== 0) {
-      arr[i] = arr[i][0].toUpperCase() + arr[i].slice(1);
-    }
-  }
+  arr = arr.map((el, index) => index !== 0 ? el[0].toUpperCase() + el.slice(1) : el);
   return arr.join('');
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,11 +1,4 @@
 function showSalary(users, age) {
-  let str = '';
-  users = users.filter(user => user.age <= age);
-  for (let i = 0; i < users.length; i++) {
-    str += `${users[i].name}, ${users[i].balance}`;
-    if (i + 1 < users.length) {
-      str += '\n';
-    }
-  }
-  return str;
+  const filteredUsers = users.filter(user => user.age <= age);
+  return filteredUsers.map(user => `${user.name}, ${user.balance}`).join('\n');
 }
