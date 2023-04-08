3fdf6beccd258bf22457a7af8c4310aa7d7d32f5
v.mikheeva <algaeminor@gmail.com>
2023-04-09 00:18:24 +0400

    5-module 1,2,3 tasks

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,4 @@
 function hideSelf() {
-  // ваш код...
+  const btn = document.querySelector('.hide-self-button');
+  btn.hidden = true;
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel__inner');
+  const slideWidth = carousel.offsetWidth;
+  const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  let slideIndex = 0;
+
+  // Скрываем кнопку "назад", т.к. мы видим первый слайд
+  carouselArrowLeft.style.display = 'none';
+
+  // Обработчик события для кнопки "вперед"
+  carouselArrowRight.addEventListener('click', () => {
+    slideIndex++;
+    carousel.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+    carouselArrowLeft.style.display = '';
+    if (slideIndex === 3) {
+      carouselArrowRight.style.display = 'none';
+    }
+  });
+
+  // Обработчик события для кнопки "назад"
+  carouselArrowLeft.addEventListener('click', () => {
+    slideIndex--;
+    carousel.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+    carouselArrowRight.style.display = '';
+    if (slideIndex === 0) {
+      carouselArrowLeft.style.display = 'none';
+    }
+  });
 }
